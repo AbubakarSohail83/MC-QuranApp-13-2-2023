c9131dc1097b02fd2141375662911974fe6faea1
Abubakar Sohail <abubakarsohail83@gmail.com>
2023-02-13 03:15:09 +0500

    Created a new layout file for listing the views

--- app/src/main/java/com/example/quranapp/mainViewAdapter.java
+++ app/src/main/java/com/example/quranapp/mainViewAdapter.java
@@ -7,21 +7,32 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class mainViewAdapter extends RecyclerView.Adapter<mainViewAdapter.mainViewHolder> {
+
+    /* Adapter class is made to adapt the data provided by the data source into the view holder class */
+    private String[] data;
+    public mainViewAdapter(String [] data)   //constructor
+    {
+        this.data=data;
+    }
+
+
+
+
     @NonNull
     @Override
 
-    public mainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public mainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //It will create view holder and store views in them
         return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull mainViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull mainViewHolder holder, int position) { //this method will bind the views with data
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int getItemCount() {     //tells the count of data in recycler View's Adapter
+        return data.length;
     }
 
     public class mainViewHolder extends RecyclerView.ViewHolder{
